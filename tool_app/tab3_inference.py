464c4a4152ab3827697972af421935105d5f5a11
ClaudiaYao <claudia.yao2012@gmail.com>
2025-04-30 01:15:28 +0800

    update to add user scoring area - need test on Brina side since some agent code could not run here

--- tool_app/tab3_inference.py
+++ tool_app/tab3_inference.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import threading
 from PIL import Image
-import time, sys
-from pathlib import Path
+import time
+import random, os
+import pandas as pd
 
 def inference(agent, image, user_prompt):
     # mock data
@@ -14,31 +14,52 @@ def inference(agent, image, user_prompt):
     return agent.infer(image, user_prompt)
 
 
-cur_dir = Path(sys.argv[0])
-base_dir = str(cur_dir.parent.resolve())
-VIDEO_DIR = base_dir + "/results"
+# add new annotation record to the existing xx_score.csv
+def add_new_record(clip_score, tc_score, dd_score, user_value, df_scores):
+    
+    if clip_score and tc_score and dd_score and user_value:
+        df_scores.loc[df_scores.shape[0]] = ["user_generated", clip_score,
+                                                                tc_score,
+                                                                dd_score,
+                                                                user_value]
+        df_scores.to_csv(f"user_marks/user_{st.session_state.current_user}_scores.csv", index=False)
 
-# Function to reset text when a new image is uploaded
+def get_score_df():
+    score_file = f"user_marks/user_{st.session_state.current_user}_scores.csv"
+    if os.path.exists(score_file):
+        df_scores = pd.read_csv(score_file)
+    else:
+        if not os.path.exists("user_marks"):
+            os.makedirs("user_marks")
+        df_scores = pd.DataFrame(columns=["file_name", "clip_tva_score", "temporal_consistency", "dynamic_degree", "user_value"])
+        df_scores.to_csv(score_file, index=False)
+    return df_scores
+
 def on_file_upload():
-    st.session_state.user_input = ""  # Clear text input
+    st.session_state.user_input = "" 
+        
 
-
-def show(agent):
+def show():
     st.subheader("🖼️ Upload Image and Add Prompt")
     
-    # Image uploader
-    uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"], on_change=on_file_upload)
+    print("fresh")
+    
+    if "current_user" not in st.session_state:
+        user_id = time.strftime("%Y%m%d%H%M%S")
+        st.session_state.current_user = user_id
     if "user_input" not in st.session_state:
         st.session_state.user_input = ""
+    if "show_score_area" not in st.session_state:
+        st.session_state.show_score_area = False
+        
+    # Image uploader
+    uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"], on_change=on_file_upload)
 
     if uploaded_file:
-        st.session_state.user_input
-        st.image(uploaded_file, caption="Image uploaded!", use_container_width=True)
+        st.image(uploaded_file, caption="Image uploaded!", use_column_width=True)
         img = Image.open(uploaded_file).convert("RGB")
 
-        # Text input
         user_prompt = st.text_input("Enter a prompt for the video you'd like to generate: ", key="user_input")
-        
         if user_prompt:
             msg = st.info("Generating video ...")
             best_video, best_prompt, clip_score, tc_score, dd_score = inference(agent, img, user_prompt)
@@ -46,3 +67,20 @@ def show(agent):
             
             if best_video != "":
                 st.video(open(best_video, 'rb').read())
+                st.markdown(f"🚀 AI generated prompt: {best_prompt}")
+            
+            if random.randint(0, 2) == 0 or st.session_state.show_score_area:
+                st.subheader("✨ We’d appreciate it if you could rate the video quality.")
+                st.session_state.show_score_area = True
+                slider_value = st.slider("**0 - poorest quality, 10 - highest quality**", min_value=0.0, max_value=10.0, step=0.1, value = 5.0, key="user_value")
+                
+                if st.button("Confirm Score", key="confirm_btn"):
+                    if "df_scores" not in st.session_state:
+                        st.session_state.df_scores = get_score_df()
+                    add_new_record(clip_score, tc_score, dd_score, slider_value, st.session_state.df_scores)
+
+                    st.session_state.show_score_area = False
+                    msg = st.success("Thank you for your scoring!")
+                    time.sleep(1.5)
+                    msg.empty()   
+                    
