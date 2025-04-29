fix: normalization and prediction range

tool_app/agent.py
@@ -63,7 +63,7 @@ class PromptPilot:
             "dynamic_degree": 0}
 
         self.reward_model = SimpleNN()
-        self.reward_model.load_state_dict(torch.load('user_marks/reward_model_state_dict_NN.pth'))
+        self.reward_model.load_state_dict(torch.load('user_marks/model/reward_model_state_dict_NN.pth'))
         self.reward_model.eval()
 
     def extract_frames(self,video_path: str, num_frames: int = 4) -> List[Image.Image]:
@@ -258,7 +258,7 @@ class PromptPilot:
                             1. CLIP Alignment Score, which measures how well the generated video frames align with the given text prompt. If CLIP Alignment is low, clarify key objects, actions or settings to help visuals match the description more precisely.
                             2. Temporal Consistency Score, which assesses the smoothness and coherence between consecutive video frames. If Temporal Consistency is low, simplify actions or reduce ambiguity that might lead to jittery or incoherent motion.
                             3. Dynamic Degree Score quantifies the amount of motion in the video by comparing pixel changes across frames. If Dynamic Degree is too high for a static scene, remove unnecessary verbs or motion elements. If Dynamic Degree is too low for an action scene, enhance verbs or scene dynamics. For example, "a cat walking" → "a cat jumping between rooftops".
-                            4. Human Score is the aggregated score for overall quality. If Human Score is low, balance all factors with clearer, more focused language.
+                            4. Human Score is the aggregated score between 0-10 for overall quality. If Human Score is low, balance all factors with clearer, more focused language.
                         """ + \
                         self.USER_PROMPT + \
                         f"""

tool_app/reward_model.py
@@ -24,6 +24,8 @@ class SimpleNN(nn.Module):
         x = torch.relu(self.fc1(x))   # Activation after 1st layer
         x = torch.relu(self.fc2(x))   # Activation after 2nd layer
         x = self.fc3(x)               # No activation at output
+        x = torch.clamp(x, 0, 10)
+        # x = torch.sigmoid(self.fc3(x)) * 10  # Sigmoid output scaled to [0, 10]
         return x
 
 def train_NN(train_df, test_df, user_stats, model_saved_path):
@@ -76,19 +78,21 @@ def train_NN(train_df, test_df, user_stats, model_saved_path):
         
     # return model
     model.eval()
-    prediction = model(X_test_tensor)
+    prediction = model(X_test_tensor).detach().numpy() 
         
     test_result = X_test.copy()
     test_result['y_real'] = y_test_tensor
     
-    y_pred_transformed = []
-    for index, pred in enumerate(prediction.detach().numpy()):
-        user_id = test_df.iloc[index]["user_id"]
-        user_min = user_stats[user_id][0]
-        user_std_or_range = user_stats[user_id][1]
-        y_pred_transformed.append(user_min + pred * user_std_or_range)
+    # y_pred_transformed = []
+    # # for index, pred in enumerate(prediction.detach().numpy()):
+    # for index, pred in enumerate(prediction):
+    #     user_id = test_df.iloc[index]["user_id"]
+    #     user_min = user_stats[user_id][0]
+    #     user_std_or_range = user_stats[user_id][1]
+    #     y_pred_transformed.append(user_min + pred * user_std_or_range)
         
-    test_result['y_pred'] = y_pred_transformed 
+    # test_result['y_pred'] = y_pred_transformed 
+    test_result['y_pred'] = prediction
     
     # Plot loss curve
     plt.plot(losses)
@@ -98,7 +102,8 @@ def train_NN(train_df, test_df, user_stats, model_saved_path):
     plt.grid()
     plt.show()
     
-    return model_saved_path, test_result, mean_squared_error(y_test, y_pred_transformed)
+    # return model_saved_path, test_result, mean_squared_error(y_test, y_pred_transformed)
+    return model_saved_path, test_result, mean_squared_error(y_test, prediction)
 
 
 def train_LR(train_df, test_df, user_stats, save_path):